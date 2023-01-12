4db6234e8a04491536be8e421786314ebd1b4f48
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-01-12 17:19:50 +0300

    Initial commit

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -1,19 +1,10 @@
 package ru.otus;
 
-import com.google.common.collect.Lists;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.collect.ImmutableSet;
 
 public class HelloOtus {
     public static void main(String... args) {
-        List<Integer> example = new ArrayList<>();
-        int min = 0;
-        int max = 100;
-        for (int i = min; i < max; i++) {
-            example.add(i);
-        }
-
-        System.out.println(Lists.reverse(example));
+        ImmutableSet<String> is = ImmutableSet.of("A","B","C");
+        is.forEach(System.out::println);
     }
 }
