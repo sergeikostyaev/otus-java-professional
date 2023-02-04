49376c287e0a5a307e37022baf43c953bf29b8e4
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-02-04 10:00:15 +0300

    Initial commit

---
+++ hw04-gc/src/main/java/ru/otus/Summator.java
@@ -0,0 +1,47 @@
+package ru.otus;
+
+public class Summator {
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private int size = 0;
+
+    public void calc(Data data) {
+
+        size++;
+
+        sum += data.getValue();
+
+        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+
+        prevPrevValue = prevValue;
+        prevValue = data.getValue();
+
+        for (var idx = 0; idx < 3; idx++) {
+            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
+            someValue = Math.abs(someValue) + size;
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public int getPrevValue() {
+        return prevValue;
+    }
+
+    public int getPrevPrevValue() {
+        return prevPrevValue;
+    }
+
+    public int getSumLastThreeValues() {
+        return sumLastThreeValues;
+    }
+
+    public int getSomeValue() {
+        return someValue;
+    }
+}
