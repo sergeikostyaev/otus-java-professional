2a0f05e6b0a029bcc297927531f4db973cb7d9ae
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-02-12 20:01:32 +0300

    Initial commit

--- hw05-logging/src/main/java/ru/otus/Ioc.java
+++ hw05-logging/src/main/java/ru/otus/Ioc.java
@@ -43,6 +43,7 @@ public class Ioc {
                                 System.out.print(o + " ");
                             }
                             System.out.println(" ");
+                            break;
                         }
                     }
 
