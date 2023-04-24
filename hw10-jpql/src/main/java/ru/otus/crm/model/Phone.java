19e06a68b4fd1767e54f4d713cf5b4e77292e401
sergeikostyaev <sergei.kostyaev88@gmail.com>
2023-04-24 10:32:48 +0300

    Initial commit

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,31 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "phone")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private Long id;
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+}
